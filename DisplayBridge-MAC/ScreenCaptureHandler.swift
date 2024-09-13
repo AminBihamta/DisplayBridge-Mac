10851094890d6a6a339eb94a3f74571df7efc89a
Amin Bihamta <97396978+AminBihamta@users.noreply.github.com>
2024-09-13 17:21:09 +0800

    screen capturing with a preview implemented

---
+++ DisplayBridge-MAC/ScreenCaptureHandler.swift
@@ -0,0 +1,8 @@
+//
+//  ScreenCaptureHandler.swift
+//  DisplayBridge-MAC
+//
+//  Created by Amin Bihamta on 13/09/2024.
+//
+
+import Foundation
---
+++ Source.swift
@@ -0,0 +1,8 @@
+//
+//  Source.swift
+//  
+//
+//  Created by Amin Bihamta on 09/09/2024.
+//
+
+import Foundation
